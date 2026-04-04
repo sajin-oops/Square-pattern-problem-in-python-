dd8c512389244ec522f953d513a8e59268d5c0ac
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2026-04-04 20:18:04 +0530

    Add files via upload

--- practice code.py
+++ practice code.py
@@ -1,8 +1,8 @@
-# n = 4
-# for i in range(n):
-#     for j in range(n):
-#         print("*",end = "")
-#     print()
+n = 4
+for i in range(n):
+    for j in range(n):
+        print("*",end = "")
+    print()
 
 '''
 O/P
@@ -27,5 +27,20 @@ for i in range(1,n+1):
 **
 ***
 ****
+
+'''
+
+n = 4
+
+for i in range(6):
+    print(i * "*")
+
+'''
+
+*
+**
+***
+****
+*****
 
 '''
